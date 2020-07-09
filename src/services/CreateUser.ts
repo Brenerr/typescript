42f562686fc0374337052cb88e7a17e7467343d9
Brener Alves <brener820@gmail.com>
2020-07-09 16:44:38 -0300

    typescript

---
+++ src/services/CreateUser.ts
@@ -0,0 +1,20 @@
+interface techObject{
+    title: string;
+    experience: number;
+}
+
+interface CreateUserData{
+    name?: string;
+    email: string;
+    password: string;
+    techs: Array<String | techObject>;
+}
+
+export default function createUser({name,email, password}: CreateUserData){
+    const user = {
+        name,
+        email,
+        password,
+    }
+    return user;
+}
